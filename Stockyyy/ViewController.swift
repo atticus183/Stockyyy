5b53a1e24353f264fa19a4e48e4a4415198ff2a6
Josh R <seventytwoapp@gmail.com>
2020-11-13 19:22:58 -0600

    Initial Commit

---
+++ Stockyyy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stockyyy
+//
+//  Created by Josh R on 11/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
